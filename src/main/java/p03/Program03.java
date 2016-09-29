c4f10058d895a92ef23f5c013567b2e505bdc337
bairavisingaravelu <bairavi13@gmail.com>
2016-09-29 15:40:19 +0530

    changes made

--- src/main/java/p03/Program03.java
+++ src/main/java/p03/Program03.java
@@ -5,14 +5,11 @@ import java.util.HashMap;
 /**
  * Created by BSIN on 9/27/2016.
  */
-public class Program03 extends RuntimeException{
-//    public static void main(String [] args){
-//      Program03 p3=new Program03();
-//        p3.calulate("hello");
-////    }
+public class Program03{
+
     public void calulate(String value) {
-        int i = 1;
-        String k = null;
+        int i = 1,n=0;
+        String k = "";
         String text = value.toUpperCase();
         int length = text.length();
         System.out.println(text + length);
@@ -22,23 +19,14 @@ public class Program03 extends RuntimeException{
             System.out.println("key " + alphabet + " value " + i);
             i++;
         }
-
-
         Character[] array = new Character[length];
         for (int j = 0; j < length; j++) {
             array[j] = new Character(text.charAt(j));
-            System.out.println(j);
-            // k = k + data.get(i);
-        }
-        for(int x=0;x<array.length;x++)
-        {
-//System.out.print(array[x]);
-            System.out.print(data.get(array[x]));
-            k=k+data.get(array[x]);
 
         }
-        System.out.println(k);
-        int n=0;
+        for(int x=0;x<array.length;x++)
+            k=k+data.get(array[x]);
+        System.out.println("result: "+k);
         char[] ch=k.toCharArray();
         for (char c:ch) {
             n=n+Integer.parseInt(String.valueOf(c));
